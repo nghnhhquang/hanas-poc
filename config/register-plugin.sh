a5d6b162793925a4a3f71947521af347a612e325
Quang Nguyen <quangnh@katalyst.vn>
2024-09-13 14:48:30 +0700

    update-21

---
+++ config/register-plugin.sh
@@ -0,0 +1,29 @@
+vault delete hanas.io/static-roles/confluent-admin
+vault delete hanas.io/config/confluent
+
+vault delete hanas.io/static-roles/datahub-admin
+vault delete hanas.io/config/datahub
+
+vault delete hanas.io/static-roles/dataiku-admin
+vault delete hanas.io/config/dataiku
+
+vault delete hanas.io/static-roles/rhpam-admin
+vault delete hanas.io/config/rhpam
+
+vault delete hanas.io/static-roles/workflow-admin
+vault delete hanas.io/config/workflow
+
+plugin_sha256="$(sha256sum /vault/plugin/hcpv-dbplugin-confluent | gawk '{ print $1 }')"
+vault plugin register -sha256=$plugin_sha256 -version=v1.0.0 -args=--with-glibc,--with-cgo database hcpv-dbplugin-confluent
+
+plugin_sha256="$(sha256sum /vault/plugin/hcpv-dbplugin-datahub | gawk '{ print $1 }')"
+vault plugin register -sha256=$plugin_sha256 -version=v1.0.0 -args=--with-glibc,--with-cgo database hcpv-dbplugin-datahub
+
+plugin_sha256="$(sha256sum /vault/plugin/hcpv-dbplugin-dataiku | gawk '{ print $1 }')"
+vault plugin register -sha256=$plugin_sha256 -version=v1.0.0 -args=--with-glibc,--with-cgo database hcpv-dbplugin-dataiku
+
+plugin_sha256="$(sha256sum /vault/plugin/hcpv-dbplugin-rhpam | gawk '{ print $1 }')"
+vault plugin register -sha256=$plugin_sha256 -version=v1.0.0 -args=--with-glibc,--with-cgo database hcpv-dbplugin-rhpam
+
+plugin_sha256="$(sha256sum /vault/plugin/hcpv-dbplugin-workflow | gawk '{ print $1 }')"
+vault plugin register -sha256=$plugin_sha256 -version=v1.0.0 -args=--with-glibc,--with-cgo database hcpv-dbplugin-workflow
