478c7e1adf315c33eff549309430a15b1d48208c
Quang Nguyen <quangnh@katalyst.vn>
2024-09-18 15:29:36 +0700

    update-101

--- hcp-plugin/db-roles-test.sh
+++ hcp-plugin/db-roles-test.sh
@@ -1,4 +1,9 @@
 vault read hanas.io/static-roles/datahub-admin
+vault read hanas.io/static-creds/datahub-admin
+
 vault read hanas.io/static-roles/dataiku-admin
+vault read hanas.io/static-creds/dataiku-admin
+
 vault read hanas.io/static-roles/workflow-admin
+vault read hanas.io/static-creds/workflow-admin
 
