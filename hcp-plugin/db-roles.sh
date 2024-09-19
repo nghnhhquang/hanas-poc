8566ba5e6becd96e12ba904e05e244b89a182319
Quang Nguyen <quangnh@katalyst.vn>
2024-09-19 14:41:29 +0700

    update-106

--- hcp-plugin/db-roles.sh
+++ hcp-plugin/db-roles.sh
@@ -4,8 +4,8 @@ vault write hanas.io/config/dataiku plugin_name="hcpv-dbplugin-dataiku" allowed_
 vault write hanas.io/config/rhpam plugin_name="hcpv-dbplugin-rhpam" allowed_roles="*" url="http://192.168.128.41:5501" username="portaladmin" password="12345" pwd="12345" insecure_tls=true
 vault write hanas.io/config/workflow plugin_name="hcpv-dbplugin-workflow" allowed_roles="*" url="http://192.168.121.42:5001" pamurl="http://192.168.128.41:5501" username="portaladmin" password="12345" insecure_tls=true
 
-vault write hanas.io/static-roles/datahub-admin db_name="datahub" username="portaladmin" rotation_period="5m" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"Admin\"]}"
-vault write hanas.io/static-roles/dataiku-admin db_name="dataiku" username="portaladmin" rotation_period="5m" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"administrators\"]}"
-vault write hanas.io/static-roles/workflow-admin db_name="workflow" username="admin" rotation_period="5d" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"2\"]}"
+vault write hanas.io/static-roles/datahub-admin db_name="datahub" username="portaladmin" rotation_period="1d" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"Admin\"]}"
+vault write hanas.io/static-roles/dataiku-admin db_name="dataiku" username="portaladmin" rotation_period="1d" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"administrators\"]}"
+vault write hanas.io/static-roles/workflow-admin db_name="workflow" username="admin" rotation_period="1d" rotation_statements="{\"allow_create_user\":true,\"roles\":[\"2\"]}"
 
 ./db-roles-test.sh
